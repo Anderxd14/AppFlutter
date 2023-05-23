fc70dd123db277f5e8f9ed48916b9539fc920334
Anderson Garnica <steven14.as@gmail.com>
2023-05-23 08:03:03 -0500

    ALERTA MAIN

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// ignore: unused_import
 import 'package:flutter_application_1/pages/formJardinero.dart';
 // ignore: unused_import
 import 'package:flutter_application_1/pages/pagesHumedad.dart';
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'EcoRoot',
-      home: FormJardineros(),
+      home: MisPlantas(),
     );
   }
 }
